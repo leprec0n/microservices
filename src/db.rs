0e317f78646f0347ed28c08263d36ba866a92cd2
jarnoweemen <jarno.weemen@gmail.com>
2024-04-17 10:41:07 +0200

    Store verification session

--- src/db.rs
+++ src/db.rs
@@ -1,5 +1,9 @@
 use std::collections::HashMap;
 
+use chrono::{Duration, Local};
+use tokio_postgres::Row;
+use tracing::debug;
+
 pub fn generate_db_conn(params: &HashMap<&str, &String>) -> String {
     format!(
         "postgresql://{user}:{password}@{host}/{db}",
@@ -8,4 +12,36 @@ pub fn generate_db_conn(params: &HashMap<&str, &String>) -> String {
         host = params.get("host").unwrap(),
         db = params.get("db").unwrap(),
     )
+}
+
+pub async fn verification_already_send(db_client: &tokio_postgres::Client, email: &str) -> bool {
+    match db_client
+        .query_one(
+            "SELECT * FROM email WHERE expires > now() AND email=$1 ORDER BY expires DESC LIMIT 1",
+            &[&email],
+        )
+        .await
+    {
+        Err(e) => {
+            debug!("{:?}", e);
+            return false;
+        }
+        _ => (),
+    };
+
+    true
+}
+
+pub async fn create_verification_session(
+    db_client: &tokio_postgres::Client,
+    email: &str,
+) -> Result<Vec<Row>, tokio_postgres::Error> {
+    let expires = 3600; // 60 minutes
+
+    Ok(db_client
+        .query(
+            "INSERT INTO email(email, expires) VALUES($1, $2)",
+            &[&email, &(Local::now() + Duration::seconds(expires))],
+        )
+        .await?)
 }
