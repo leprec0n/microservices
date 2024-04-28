8d87b55b4eb187cfc1fb206aee03b5d683858c73
jarnoweemen <jarno.weemen@gmail.com>
2024-04-28 13:55:54 +0200

    Update migration

--- src/account/migrations/V1__initial.rs
+++ src/account/migrations/V1__initial.rs
@@ -14,16 +14,33 @@ pub fn migration() -> String {
         t.add_column("balance", types::double());
         t.add_column("currency_id", types::integer());
 
-        t.add_foreign_key(&["currency_id"], "currencies", &["id"])
+        t.add_foreign_key(&["currency_id"], "currencies", &["id"]);
+    });
+
+    m.create_table_if_not_exists("customer_details", |t| {
+        t.add_column("id", types::primary());
+        t.add_column("first_name", types::text().nullable(true));
+        t.add_column("middle_name", types::text().nullable(true));
+        t.add_column("last_name", types::text().nullable(true));
+        t.add_column("postal_code", types::text().nullable(true));
+        t.add_column("street_name", types::text().nullable(true));
+        t.add_column("street_nr", types::text().nullable(true));
+        t.add_column("premise", types::text().nullable(true));
+        t.add_column("settlement", types::text().nullable(true));
+        t.add_column("country", types::text().nullable(true));
+        t.add_column("country_code", types::text().nullable(true));
+        t.add_column("user_id", types::integer().nullable(true));
+
+        t.add_foreign_key(&["user_id"], "users", &["id"]);
     });
 
     m.create_table_if_not_exists("sessions", |t| {
         t.add_column("id", types::primary());
         t.add_column("expires", types::custom("timestamp with time zone"));
         t.add_column("type", types::text());
-        t.add_column("sub_id", types::integer());
+        t.add_column("user_id", types::integer());
 
-        t.add_foreign_key(&["sub_id"], "users", &["id"]);
+        t.add_foreign_key(&["user_id"], "users", &["id"]);
     });
 
     m.make::<Pg>()
